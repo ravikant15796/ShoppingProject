dd6ef3080dd9bd5e0067fc8d2a243e32a73826d8
Ravikant Yadav <ravikant@Ravikants-Air.lan>
2018-07-19 23:04:25 +0530

    commit 19

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes =[];
+recipes Recipe[] =[
+  new Recipe('A Test Recipe' , 'This simply a test' , 'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
+];
 
  constructor() { }
 
