1ffa8412f16bd1659c2ff6446594c9930f069983
Ravikant Yadav <ravikant@Ravikants-Air.lan>
2018-08-03 06:55:18 +0530

    commit 2 Aug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+   loadedFeature = 'recipe';
+ navigateUrl(data:string)
+ {
+
+   this.loadedFeature = data;  
+
+ }
+
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+ 
 
 @Component({
     selector:'app-header',
@@ -6,5 +7,13 @@ import { Component } from "@angular/core";
 
 })
 export class HeaderComponent{
+   
+    @Output() selectedFeature = new EventEmitter<string>();
+
+    onSelect(feature:string)
+    {
+        
+      this.selectedFeature.emit(feature);
+    }
 
 }
